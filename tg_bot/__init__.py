bf3b76b3d82d790cd3466ff803ae50986a550f8a
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-03 15:05:16 +0500

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -39,7 +39,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 7:
 
 parser = ConfigParser()
 parser.read("config.ini")
-config = parser["config"]
+Kigconfig = parser["configparser"]
 
 class KigyoINIT:
     def __init__(self, parser):
